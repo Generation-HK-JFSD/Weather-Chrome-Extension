271fb223a882f5d9565835e32145e08bb12f69ad
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2025-04-07 23:58:13 +0800

    Add current weather page

---
+++ src/CurrentWeather.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+
+function CurrentWeather() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const url =
+      'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc';
+    fetch(url)
+      .then((res) => {
+        console.log('res', res);
+        return res.json();
+      })
+      .then((obj) => {
+        console.log('obj', obj);
+        setWeatherData(obj);
+      });
+  }, []);
+
+  // let { uvindex, humidity, updateTime } = weatherData;
+  let uvindex = weatherData?.uvindex;
+  let humidity = weatherData?.humidity;
+  let updateTime = weatherData?.updateTime ?? 'No Data';
+
+  let uvLine = 'No Data';
+  let uvMsg = 'No Data';
+  let humidityValue = 'No Data';
+
+  let warningMessage = weatherData?.warningMessage;
+  let rainstormReminder = weatherData?.rainstormReminder;
+
+  if (uvindex) {
+    uvLine = `${uvindex.data[0].value} ${uvindex.data[0].desc}`;
+    uvMsg = uvindex.data[0].message;
+  }
+  if (humidity) {
+    humidityValue = humidity.data[0].value;
+  }
+
+  return (
+    <>
+      <div id='temp'>
+        {weatherData &&
+          weatherData.temperature.data.map((el, idx) => {
+            return (
+              <p key={idx}>
+                {el.place}: {el.value}°C
+              </p>
+            );
+          })}
+      </div>
+      <div id='uvindex'>
+        <h2>UV Index</h2>
+        <p>UV Index Value: {uvLine}</p>
+        <p>UV Message: {uvMsg}</p>
+      </div>
+      <div>
+        <p>Humidity: {humidityValue}</p>
+      </div>
+      <div>
+        <p>Warning Message: {warningMessage}</p>
+        <p>rainstormReminder: {rainstormReminder}</p>
+      </div>
+      <div>
+        <p>Update Time: {updateTime}</p>
+      </div>
+    </>
+  );
+}
+
+export default CurrentWeather;
