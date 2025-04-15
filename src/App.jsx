781072d247baede5af6ccf6c4fb5a87f420b500e
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2025-04-15 13:53:43 +0800

    Clear code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css';
 import CurrentWeather from './CurrentWeather';
-import WeatherByLocation from './WeatherByLocation';
-// import Test from './Test';
 
 function App() {
   return (
     <>
-      {/* <WeatherByLocation /> */}
-      {/* <hr /> */}
       <CurrentWeather />
-      {/* <Test /> */}
     </>
   );
 }
