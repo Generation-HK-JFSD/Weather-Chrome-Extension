18d1bc7d5ee09dfa8fc86288972ba6768497d701
dhhchui <donald.chui@yahoo.com>
2025-04-15 14:51:35 +0800

    First iteration of forecast section

---
+++ src/Data.jsx
@@ -0,0 +1,13 @@
+function Data({ param, value, unit }) {
+  return (
+    <div className="flex-grow flex flex-col">
+      <p className="text-base text-left">{param}</p>
+      <div className="flex items-center gap-1">
+        <p className="text-2xl">{value}</p>
+        <p className="text-base">{unit}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Data;
