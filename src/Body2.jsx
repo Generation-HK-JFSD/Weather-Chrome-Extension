b57cd7b59d128e6e4d16ac64746c5464dfb92478
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2025-04-16 00:35:00 +0800

    Merge update time and warning message

--- src/Body2.jsx
+++ src/Body2.jsx
@@ -86,7 +86,8 @@ function Body2() {
   let uvMsg = 'No Data';
   let humidityValue = 'No Data';
 
-  let warningMessage = weatherData?.warningMessage;
+  // warningMessage from response will be either array of string or ""
+  let warningMessages = weatherData?.warningMessage;
   let rainstormReminder = weatherData?.rainstormReminder;
 
   if (uvindex) {
@@ -100,33 +101,64 @@ function Body2() {
   }
 
   return (
-    <div className='flex justify-between items-center gap-6'>
-      <div className='flex-grow flex flex-col gap-4'>
-        <div className='flex flex-col gap-1'>
-          <Select
-            options={locationOptions}
-            onChangeHandler={locationOnChangeHandler}
-            selectedValue={selectedLocation}
-            className='self-start bg-[rgba(0,0,0,0.1)] rounded-lg px-3 py-2 border-r-12 border-transparent text-2xl'
-          />
+    <>
+      <div className='flex justify-between items-center gap-6'>
+        <div className='flex-grow flex flex-col gap-4'>
+          <div className='flex flex-col gap-1'>
+            <Select
+              options={locationOptions}
+              onChangeHandler={locationOnChangeHandler}
+              selectedValue={selectedLocation}
+              className='self-start bg-[rgba(0,0,0,0.1)] rounded-lg px-3 py-2 border-r-12 border-transparent text-2xl'
+            />
 
-          <div className='flex items-center gap-1 px-4'>
-            <p className='text-5xl'>{tempOfSelectedLocation}</p>
-            <p className='text-2xl'>°C</p>
+            <div className='flex items-center gap-1 px-4'>
+              <p className='text-5xl'>{tempOfSelectedLocation}</p>
+              <p className='text-2xl'>°C</p>
+            </div>
+          </div>
+
+          <div className='grid grid-cols-2 grid-rows-2 gap-4 bg-[rgba(0,0,0,0.1)] p-4 rounded-lg'>
+            <Data param='相對濕度' value={humidityValue} unit='%' />
+            <Data param='雨量' value={maxRainfallDisplay} unit='mm' />
+            <Data param='紫外線指數' value={uvValue} unit={uvLevel} />
+            <Data param='空氣污染指數' value='9' unit='甚高' />
           </div>
         </div>
-
-        <div className='grid grid-cols-2 grid-rows-2 gap-4 bg-[rgba(0,0,0,0.1)] p-4 rounded-lg'>
-          <Data param='相對濕度' value={humidityValue} unit='%' />
-          <Data param='雨量' value={maxRainfallDisplay} unit='mm' />
-          <Data param='紫外線指數' value={uvValue} unit={uvLevel} />
-          <Data param='空氣污染指數' value='9' unit='甚高' />
-        </div>
+        {/* <i className='wi wi-day-cloudy text-[168px] px-3 py-12 opacity-20'></i> */}
+        {/* TODO add css for weather icon */}
+        <WeatherIcon iconNum={weatherData?.icon} className='w-50' />
       </div>
-      {/* <i className='wi wi-day-cloudy text-[168px] px-3 py-12 opacity-20'></i> */}
-      {/* TODO add css for weather icon */}
-      <WeatherIcon iconNum={weatherData?.icon} className='w-50' />
-    </div>
+
+      {/* TODO Review the icon of warning messages */}
+      <div className='flex flex-col gap-4 bg-[rgba(0,0,0,0.1)] p-4 rounded-lg'>
+        {Array.isArray(warningMessages) === false
+          ? null
+          : warningMessages.map((el) => {
+              <div className='flex gap-4 item-center'>
+                <img src='./assets/warning-symbols/hot.png'></img>
+                <p>{el}</p>
+              </div>;
+            })}
+
+        {/* ===== warning message for dummy data ===== */}
+        {/* <div className='flex gap-4 item-center'>
+          <img src='./assets/warning-symbols/hot.png'></img>
+          <p>香港天文台在下午1時45分發出酷熱天氣警告。</p>
+        </div>
+        <div className='flex gap-4 item-center'>
+          <img src='./assets/warning-symbols/hot.png'></img>
+          <p>現時之火災危險警告為紅色，表示火災危險性極高。</p>
+        </div> */}
+      </div>
+
+      <div className='flex gap-4 justify-center items-center'>
+        {/* TODO format update time */}
+        <p>更新時間: {updateTime}</p>
+        <p className='bg-[rgba(0,0,0,0.1)] px-2 py-1 rounded-lg'>香港天文台</p>
+        <p className='bg-[rgba(0,0,0,0.1)] px-2 py-1 rounded-lg'>環境保護署</p>
+      </div>
+    </>
   );
 }
 
--- src/Extension.jsx
+++ src/Extension.jsx
@@ -15,25 +15,8 @@ function Extension() {
         {/* <p className="bg-[rgba(0,0,0,0.1)] rounded-lg px-2 py-1">English</p> */}
       </div>
 
-      {/* ==== To be replace by Body2 START === */}
       <Body2 />
-      {/* ==== To be replace by Body2 END === */}
 
-      <div className='flex flex-col gap-4 bg-[rgba(0,0,0,0.1)] p-4 rounded-lg'>
-        <div className='flex gap-4 item-center'>
-          <img src='./assets/warning-symbols/hot.png'></img>
-          <p>香港天文台在下午1時45分發出酷熱天氣警告。</p>
-        </div>
-        <div className='flex gap-4 item-center'>
-          <img src='./assets/warning-symbols/hot.png'></img>
-          <p>現時之火災危險警告為紅色，表示火災危險性極高。</p>
-        </div>
-      </div>
-      <div className='flex gap-4 justify-center items-center'>
-        <p>更新時間: 2025年4月7日 16:00</p>
-        <p className='bg-[rgba(0,0,0,0.1)] px-2 py-1 rounded-lg'>香港天文台</p>
-        <p className='bg-[rgba(0,0,0,0.1)] px-2 py-1 rounded-lg'>環境保護署</p>
-      </div>
       <div className='grid grid-rows-10 gap-4 bg-[rgba(0,0,0,0.1)] p-4 rounded-lg'>
         <div className='grid grid-cols-4 gap-4 items-center'>
           <div className='text-left'>未來九天</div>
