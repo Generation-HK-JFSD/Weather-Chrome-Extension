5703fba1bdba26a8db04e9b52085218d173ae21e
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2025-04-17 00:32:08 +0800

    Remove weather forecast description

--- src/Extension.jsx
+++ src/Extension.jsx
@@ -6,13 +6,6 @@ import NineDayWeatherForecast from './NineDayWeatherForecast';
 function Extension() {
   return (
     <div className='bg-[#397326] p-6 flex flex-col gap-4 '>
-      // TODO add refresh button. add auto refresh
-      <div className='flex justify-between gap-6'>
-        <p className='text-2xl flex-grow text-left'>
-          部分時間有陽光。吹和緩偏東風。
-        </p>
-        {/* <p className="bg-[rgba(0,0,0,0.1)] rounded-lg px-2 py-1">English</p> */}
-      </div>
       <Body2 />
       <NineDayWeatherForecast />
     </div>
