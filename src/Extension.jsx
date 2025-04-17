909848d7d8cc529ae1016e902bc861d238980fdd
dhhchui <donald.chui@yahoo.com>
2025-04-17 11:36:28 +0800

    Changed initial background color to gray; Removed warning symbol import

--- src/Extension.jsx
+++ src/Extension.jsx
@@ -1,11 +1,10 @@
 import './App.css';
-import './assets/warning-symbols/hot.png';
 import Body2 from './Body2';
 import NineDayWeatherForecast from './NineDayWeatherForecast';
 
 function Extension() {
   return (
-    <div id='extension' className='bg-[#397326] p-6 flex flex-col gap-4 '>
+    <div id='extension' className='bg-[#4c4c4c] p-6 flex flex-col gap-4 '>
       <Body2 />
       <NineDayWeatherForecast />
     </div>
